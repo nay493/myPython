feda53970ed95d37e7be70497feb4ead0bde1730
nay493 <mohammednayeem45@gmail.com>
2019-03-01 09:52:27 +0530

    Add files via upload
    Add zakaat examples

---
+++ zakaat1.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+
+#
+# example command:
+#  ./zakaat1.py  --gr 3432 --gq 10 --gu tola --sr 43  --sq 500  --su grams
+#
+#
+
+import argparse
+import os
+import sys
+
+def minimumof(x, y):
+	if x <= y:
+		return x
+	else:
+		return y
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--gr", help="gold rate: ")
+	parser.add_argument("--gq", help="gold quantity: ")
+	parser.add_argument("--gu", help="gold units: ", choices = ['grams', 'tola'], nargs='?', const=1)
+	parser.add_argument("--sr", help="silver rate: ")
+	parser.add_argument("--sq", help="silver quantity: ")
+	parser.add_argument("--su", help="silver units: ", choices = ['grams', 'tola'], nargs='?', const=1)
+	args = parser.parse_args()
+	
+	if args.gu == 'tola':
+		x = int(args.gq)
+		quantity = x*10 #convert tola to gms
+	else:
+		quantity = int(args.gq)
+	
+	total_gold = quantity * int(args.gr)
+	print("total_gold = {}".format(total_gold))
+	total_silver = int(args.sq) * int(args.sr)
+	print("total_silver = {}".format(total_silver))
+	nisaab = minimumof(total_gold, total_silver)
+	print("nisaab = {}".format(nisaab))
---
+++ zakeet1.py
@@ -0,0 +1,27 @@
+import argparse
+zakaat(amout, percentage)
+
+gold(rate_of_gold, grams)
+silver(rate_of_silver, grams)
+
+zakWajib = ["gold", "silver", "plot"]
+limit =	{'gold': '85 gm',
+		'silver': '585 gm',
+		'plot': '',
+		'stock': '',
+		'paddy': '20 mann'
+		'treasure': '20 percent'
+		'animals': '>4 5:1'
+}
+
+zakNotGivenTo = ['gairMuslim', 'tandurust', 'patienHavingMoney', 'QuarnBuy', 'kafan', 'schools', 'marriage', 'workers', 'peshawar mangnewale', 'be namaazi', 'bad aqlaakh', 'biddati', 'syed_from_nabi_khandaan', 'notForpallenewaloku', ]
+
+#
+# Reference:
+# https://www.youtube.com/watch?v=7ghVzq3t-4o
+#fasl yearly n times where n is no. of times fasl katthi hai
+# which can be stocked for years like paddy, wheat, dates, etc zakaat should be removed
+# zakaat(amount, 5) --> if water from electricity
+# zakaat(amount, 10) --> if water from Nature
+
+print limit['gold']
